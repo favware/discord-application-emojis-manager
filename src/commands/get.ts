93ee096d643ab24530a3b28ae73a27917eb6ea0d
Jeroen Claassens <support@favware.tech>
2024-07-28 17:47:43 +0200

    feat: make it possible to get by name

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -2,10 +2,11 @@ import { container } from '@sapphire/pieces';
 import type { Snowflake } from 'discord-api-types/globals';
 import { Routes } from 'discord-api-types/v10';
 import { Command } from '#lib/structures/Command';
-import { checksIdIsInArgs } from '#lib/utils/checks';
+import { checksNameOrIdIsInArgs } from '#lib/utils/checks';
 import { handleError } from '#lib/utils/error-handler';
+import { getIdForPossibleName } from '#lib/utils/get-current-emoji-names';
 
-type Args = [['id', Snowflake]];
+type Args = [['nameOrId', Snowflake]];
 
 export class GetEmojis extends Command<Args> {
 	public constructor(context: Command.LoaderContext) {
@@ -13,18 +14,21 @@ export class GetEmojis extends Command<Args> {
 			description: 'Gets a single emojis from the server in JSON format',
 			arguments: [
 				{
-					name: 'id',
-					description: 'The id of the emoji to get'
+					name: 'nameOrId',
+					description:
+						'The name or snowflake of the emoji to get. If a name is provided a list of emojis will be fetched to find the id to get.'
 				}
 			]
 		});
 	}
 
 	public override async run({ args, options }: Command.Run<Args>) {
-		checksIdIsInArgs('get', args.id);
+		checksNameOrIdIsInArgs('get', args.nameOrId);
+
+		const emojiId = await getIdForPossibleName(args.nameOrId, options);
 
 		try {
-			const response = await this.container.rest.get(Routes.applicationEmoji(options.applicationId, args.id));
+			const response = await this.container.rest.get(Routes.applicationEmoji(options.applicationId, emojiId));
 
 			this.container.logger.info(`Requested emoji data:`);
 			console.log(JSON.stringify(response, null, 4));
