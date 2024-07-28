82c6d4390c1036f607ba9e45874f45ea943151d7
Jeroen Claassens <support@favware.tech>
2024-07-28 17:48:32 +0200

    feat: implement delete

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -1,8 +1,12 @@
 import { container } from '@sapphire/pieces';
 import type { Snowflake } from 'discord-api-types/globals';
+import { Routes } from 'discord-api-types/v10';
 import { Command } from '#lib/structures/Command';
+import { checksNameOrIdIsInArgs } from '#lib/utils/checks';
+import { handleError } from '#lib/utils/error-handler';
+import { getIdForPossibleName } from '#lib/utils/get-current-emoji-names';
 
-type Args = [['id', Snowflake]];
+type Args = [['nameOrId', Snowflake]];
 
 export class DeleteEmoji extends Command<Args> {
 	public constructor(context: Command.LoaderContext) {
@@ -10,15 +14,26 @@ export class DeleteEmoji extends Command<Args> {
 			description: 'Deletes an emoji from the server',
 			arguments: [
 				{
-					name: 'id',
-					description: 'The id of the emoji to get'
+					name: 'nameOrId',
+					description:
+						'The name or snowflake of the emoji to delete. If a name is provided a list of emojis will be fetched to find the id to delete.'
 				}
 			]
 		});
 	}
 
-	public override run({ args }: Command.Run<Args>) {
-		throw new Error(`Method not implemented. id=${args.id}`);
+	public override async run({ args, options }: Command.Run<Args>) {
+		checksNameOrIdIsInArgs('get', args.nameOrId);
+
+		const emojiId = await getIdForPossibleName(args.nameOrId, options);
+
+		try {
+			await this.container.rest.delete(Routes.applicationEmoji(options.applicationId, emojiId));
+
+			this.container.logger.info(`Deleted emoji ${args.nameOrId} successfully`);
+		} catch (error) {
+			handleError(error as Error);
+		}
 	}
 }
 
