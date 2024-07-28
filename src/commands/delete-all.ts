787fcd7bf2a053e49a3ce10a5970d35e10e33690
Jeroen Claassens <support@favware.tech>
2024-07-28 17:48:48 +0200

    feat: implement delete-all

--- src/commands/delete-all.ts
+++ src/commands/delete-all.ts
@@ -1,5 +1,8 @@
 import { container } from '@sapphire/pieces';
+import { Routes } from 'discord-api-types/v10';
 import { Command } from '#lib/structures/Command';
+import { handleError } from '#lib/utils/error-handler';
+import { getCurrentEmojis } from '#lib/utils/get-current-emoji-names';
 
 export class DeleteEmojis extends Command<never> {
 	public constructor(context: Command.LoaderContext) {
@@ -8,8 +11,21 @@ export class DeleteEmojis extends Command<never> {
 		});
 	}
 
-	public override run() {
-		throw new Error('Method not implemented.');
+	public override async run({ options }: Command.Run<never>) {
+		const currentEmojis = await getCurrentEmojis(options);
+
+		const promises: Promise<unknown>[] = [];
+
+		for (const emoji of currentEmojis) {
+			promises.push(this.container.rest.delete(Routes.applicationEmoji(options.applicationId, emoji.id)));
+		}
+
+		try {
+			await Promise.all(promises);
+			this.container.logger.info(`Deleted all emoji successfully`);
+		} catch (error) {
+			handleError(error as Error);
+		}
 	}
 }
 
